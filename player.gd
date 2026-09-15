2f7443ecef83d88ef732fb5b6d87dbc5d5b1f558
vamsim17 <vamsimarrapu17@gmail.com>
2026-09-15 18:02:48 -0500

    added pipe and bird textures, added bird tilting on jump and fall,  adjusted sizing on textures

--- player.gd
+++ player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const speed = 300.0
 const jumpvel = -500.0;
+const BIRD_TEXTURE: Texture2D = preload("res://yellowbird-midflap.png")
+const TILT_ANGLE = 35.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
@@ -20,6 +22,10 @@ func _physics_process(delta: float) -> void:
 	if(Input.is_action_just_pressed("Space")):
 		velocity.y = jumpvel
 		print("space")
+	if velocity.y < 0:
+		rotation_degrees = -TILT_ANGLE
+	elif velocity.y > 0:
+		rotation_degrees = TILT_ANGLE
 	
 	move_and_slide()
 	
